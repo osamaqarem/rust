2ffed031a6807a8634c7763a160e3625316871ac
Osama Qarem <osamaqarem@gmail.com>
2024-08-03 01:45:11 +0200

    rustlings: quiz1 done

--- rustlings/exercises/quizzes/quiz1.rs
+++ rustlings/exercises/quizzes/quiz1.rs
@@ -8,9 +8,13 @@
 // - However, if Mary buys more than 40 apples, the price of each apple in the
 // entire order is reduced to only 1 rustbuck!
 
-// TODO: Write a function that calculates the price of an order of apples given
-// the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if count > 40 {
+        count
+    } else {
+        count * 2
+    }
+ }
 
 fn main() {
     // You can optionally experiment here.
